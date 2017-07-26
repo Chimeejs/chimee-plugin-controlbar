dc5e96f55cf2a1fafe834420e0ebc9de08bd9e40
yandeqiang <yandeqiang@mac.local>
2017-07-26 11:58:46 +0800

    [update] what: Why: How:

--- src/build/rollup.config.es.js
+++
@@ -1,5 +0,0 @@
-import base from './rollup.config.base';
-export default base({
-  format: 'es',
-  dest: 'lib/index.mjs',
-});
--- src/build/rollup.config.umd.js
+++
@@ -1,5 +0,0 @@
-import base from './rollup.config.base';
-export default base({
-  format: 'umd',
-  dest: 'lib/index.browser.js'
-});
